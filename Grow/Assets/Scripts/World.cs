887e9e50b6420d67a8455703afd54c999e4d5c82
John Wiggins <john.wiggins@xfel.eu>
2017-01-21 08:43:21 +0100

    Advance the month every 5 seconds instead of each frame

--- Grow/Assets/Scripts/World.cs
+++ Grow/Assets/Scripts/World.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class World : MonoBehaviour {
 
+	const float kMonthDuration = 5.0f;
+	float m_currentMonthTime;
+
 	Plant m_plant;
 	Season m_season;
 	WeatherType m_weather;
@@ -12,14 +15,22 @@ public class World : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		m_month = 1;
+		m_currentMonthTime = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// Advance the month
-		m_month = (m_month % 12) + 1;
-		// ... which might change the season
-		m_season = m_season.StepMonth(m_month);
+		m_currentMonthTime += Time.deltaTime;
+
+		// Maybe advance the month
+		if (m_currentMonthTime >= kMonthDuration) {
+			m_month = (m_month % 12) + 1;
+			m_currentMonthTime = 0.0f;
+
+			// ... which might change the season
+			m_season = m_season.StepMonth(m_month);
+		}
+
 		m_weather = m_season.CurrentWeather();
 		m_plant.ExperienceEnvironment(m_season, m_weather);
 	}
