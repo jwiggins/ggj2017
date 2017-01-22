cd8dfd142cffbd7bc2e69f98373b8cd93eba086a
John Wiggins <john.wiggins@xfel.eu>
2017-01-22 11:40:20 +0100

    Hook up the splash screen

---
+++ Grow/Assets/Scripts/SplashController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashController : MonoBehaviour {
+
+    float countdownTimer;
+
+    void Start() {
+        countdownTimer = 5f;
+    }
+
+    void Update() {
+        countdownTimer -= Time.deltaTime;
+        if (countdownTimer <= 0f) {
+            SceneManager.LoadScene("WorldDev");
+        }
+    }
+}
